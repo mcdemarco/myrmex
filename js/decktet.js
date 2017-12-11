338b7d53bb841fe343c4ebf0e9780081ca1cf84b
M. C. DeMarco <mcd@Livrozinho.local>
2017-12-11 13:04:48 -0500

    Support deck sorting for testing purposes.

--- js/decktet.js
+++ js/decktet.js
@@ -16,7 +16,7 @@ context.create = (function () {
 		deck: deck
 	};
 
-	function card(rank, suit1, suit2, suit3, name, face, image, value, deckNo) {
+	function card(rank, suit1, suit2, suit3, name, face, image, value, deckNo, cardNo) {
 		// Create and return a card object
 		return {
 			Rank: rank,
@@ -29,7 +29,8 @@ context.create = (function () {
 			FaceUp: false,
 			Value: value,
 			DeckNo: deckNo,
-			Selected: false
+			Selected: false,
+			CardNo: cardNo
 		};
 	}
 
@@ -37,51 +38,51 @@ context.create = (function () {
 		// Create a new decktet deck
 		var deck = new Array();
 		for (var d=1;d<=decks;d++) {
-			deck.push(card('Ace', 'Knots', '', '', 'Ace of Knots', false, '1_ace_knots.png', 1, d));
-			deck.push(card('Ace', 'Leaves', '', '', 'Ace of Leaves', false, '1_ace_leaves.png', 1, d));
-			deck.push(card('Ace', 'Moons', '', '', 'Ace of Moons', false, '1_ace_moons.png', 1, d));
-			deck.push(card('Ace', 'Suns', '', '', 'Ace of Suns', false, '1_ace_suns.png', 1, d));
-			deck.push(card('Ace', 'Waves', '', '', 'Ace of Waves', false, '1_ace_waves.png', 1, d));
-			deck.push(card('Ace', 'Wyrms', '', '', 'Ace of Wyrms', false, '1_ace_wyrms.png', 1, d));
-			deck.push(card('2', 'Moons', 'Knots', '', 'the AUTHOR', true, '2_author.png', 2, d));
-			deck.push(card('2', 'Suns', 'Wyrms', '', 'the DESERT', false, '2_desert.png', 2, d));
-			deck.push(card('2', 'Waves', 'Leaves', '', 'the ORIGIN', false, '2_origin.png', 2, d));
-			deck.push(card('3', 'Moons', 'Waves', '', 'the JOURNEY', false, '3_journey.png', 3, d));
-			deck.push(card('3', 'Suns', 'Knots', '', 'the PAINTER', true, '3_painter.png', 3, d));
-			deck.push(card('3', 'Leaves', 'Wyrms', '', 'the SAVAGE', true, '3_savage.png', 3, d));
-			deck.push(card('4', 'Wyrms', 'Knots', '', 'the BATTLE', false, '4_battle.png', 4, d));
-			deck.push(card('4', 'Moons', 'Suns', '', 'the MOUNTAIN', false, '4_mountain.png', 4, d));
-			deck.push(card('4', 'Waves', 'Leaves', '', 'the SAILOR', true, '4_sailor.png', 4, d));
-			deck.push(card('5', 'Suns', 'Waves', '', 'the DISCOVERY', false, '5_discovery.png', 5, d));
-			deck.push(card('5', 'Moons', 'Leaves', '', 'the FOREST', false, '5_forest.png', 5, d));
-			deck.push(card('5', 'Wyrms', 'Knots', '', 'the SOLDIER', true, '5_soldier.png', 5, d));
-			deck.push(card('6', 'Moons', 'Waves', '', 'the LUNATIC', true, '6_lunactic.png', 6, d));
-			deck.push(card('6', 'Leaves', 'Knots', '', 'the MARKET', false, '6_market.png', 6, d));
-			deck.push(card('6', 'Suns', 'Wyrms', '', 'the PENITENT', true, '6_penitent.png', 6, d));
-			deck.push(card('7', 'Suns', 'Knots', '', 'the CASTLE', false, '7_castle.png', 7, d));
-			deck.push(card('7', 'Waves', 'Wyrms', '', 'the CAVE', false, '7_cave.png', 7, d));
-			deck.push(card('7', 'Moons', 'Leaves', '', 'the CHANCE MEETING', false, '7_chance_meeting.png', 7, d));
-			deck.push(card('8', 'Wyrms', 'Knots', '', 'the BETRAYAL', false, '8_betrayal.png', 8, d));
-			deck.push(card('8', 'Moons', 'Suns', '', 'the DIPLOMAT', true, '8_diplomat.png', 8, d));
-			deck.push(card('8', 'Waves', 'Leaves', '', 'the MILL', false, '8_mill.png', 8, d));
-			deck.push(card('9', 'Waves', 'Wyrms', '', 'the DARKNESS', false, '9_darkness.png', 9, d));
-			deck.push(card('9', 'Leaves', 'Knots', '', 'the MERCHANT', true, '9_merchant.png', 9, d));
-			deck.push(card('9', 'Moons', 'Suns', '', 'the PACT', false, '9_pact.png', 9, d));
-			deck.push(card('CROWN', 'Knots', '', '', 'the WINDFALL', false, 'crown_knots.png', 10, d));
-			deck.push(card('CROWN', 'Leaves', '', '', 'the END', false, 'crown_leaves.png', 10, d));
-			deck.push(card('CROWN', 'Moons', '', '', 'the HUNTRESS', true, 'crown_moons.png', 10, d));
-			deck.push(card('CROWN', 'Suns', '', '', 'the BARD', true, 'crown_suns.png', 10, d));
-			deck.push(card('CROWN', 'Waves', '', '', 'the SEA', false, 'crown_waves.png', 10, d));
-			deck.push(card('CROWN', 'Wyrms', '', '', 'the CALAMITY', false, 'crown_wyrms.png', 10, d));
-			deck.push(card('', '', '', '', 'the EXCUSE', false, 'excuse.png', 0, d));
-			deck.push(card('PAWN', 'Waves', 'Leaves', 'Wyrms', 'the BORDERLAND', false, 'pawn_borderland.png', 11, d));
-			deck.push(card('PAWN', 'Moons', 'Suns', 'Leaves', 'the HARVEST', false, 'pawn_harvest.png', 11, d));
-			deck.push(card('PAWN', 'Suns', 'Waves', 'Knots', 'the LIGHT KEEPER', true, 'pawn_light_keeper.png', 11, d));
-			deck.push(card('PAWN', 'Moons', 'Wyrms', 'Knots', 'the WATCHMAN', true, 'pawn_watchman.png', 11, d));
-			deck.push(card('COURT', 'Moons', 'Waves', 'Knots', 'the CONSUL', true, '11_court_consul.png', 12, d));
-			deck.push(card('COURT', 'Suns', 'Waves', 'Wyrms', 'the ISLAND', false, '11_court_island.png', 12, d));
-			deck.push(card('COURT', 'Moons', 'Leaves', 'Wyrms', 'the RITE', false, '11_court_rite.png', 12, d));
-			deck.push(card('COURT', 'Suns', 'Leaves', 'Knots', 'the WINDOW', false, '11_court_window.png', 12, d));
+			deck.push(card('Ace', 'Knots', '', '', 'Ace of Knots', false, '1_ace_knots.png', 1, d, 1));
+			deck.push(card('Ace', 'Leaves', '', '', 'Ace of Leaves', false, '1_ace_leaves.png', 1, d, 2));
+			deck.push(card('Ace', 'Moons', '', '', 'Ace of Moons', false, '1_ace_moons.png', 1, d, 3));
+			deck.push(card('Ace', 'Suns', '', '', 'Ace of Suns', false, '1_ace_suns.png', 1, d, 4));
+			deck.push(card('Ace', 'Waves', '', '', 'Ace of Waves', false, '1_ace_waves.png', 1, d, 5));
+			deck.push(card('Ace', 'Wyrms', '', '', 'Ace of Wyrms', false, '1_ace_wyrms.png', 1, d, 6));
+			deck.push(card('2', 'Moons', 'Knots', '', 'the AUTHOR', true, '2_author.png', 2, d, 7));
+			deck.push(card('2', 'Suns', 'Wyrms', '', 'the DESERT', false, '2_desert.png', 2, d, 8));
+			deck.push(card('2', 'Waves', 'Leaves', '', 'the ORIGIN', false, '2_origin.png', 2, d, 9));
+			deck.push(card('3', 'Moons', 'Waves', '', 'the JOURNEY', false, '3_journey.png', 3, d, 10));
+			deck.push(card('3', 'Suns', 'Knots', '', 'the PAINTER', true, '3_painter.png', 3, d, 11));
+			deck.push(card('3', 'Leaves', 'Wyrms', '', 'the SAVAGE', true, '3_savage.png', 3, d, 12));
+			deck.push(card('4', 'Wyrms', 'Knots', '', 'the BATTLE', false, '4_battle.png', 4, d, 13));
+			deck.push(card('4', 'Moons', 'Suns', '', 'the MOUNTAIN', false, '4_mountain.png', 4, d, 14));
+			deck.push(card('4', 'Waves', 'Leaves', '', 'the SAILOR', true, '4_sailor.png', 4, d, 15));
+			deck.push(card('5', 'Suns', 'Waves', '', 'the DISCOVERY', false, '5_discovery.png', 5, d, 16));
+			deck.push(card('5', 'Moons', 'Leaves', '', 'the FOREST', false, '5_forest.png', 5, d, 17));
+			deck.push(card('5', 'Wyrms', 'Knots', '', 'the SOLDIER', true, '5_soldier.png', 5, d, 18));
+			deck.push(card('6', 'Moons', 'Waves', '', 'the LUNATIC', true, '6_lunactic.png', 6, d, 19));
+			deck.push(card('6', 'Leaves', 'Knots', '', 'the MARKET', false, '6_market.png', 6, d, 20));
+			deck.push(card('6', 'Suns', 'Wyrms', '', 'the PENITENT', true, '6_penitent.png', 6, d, 21));
+			deck.push(card('7', 'Suns', 'Knots', '', 'the CASTLE', false, '7_castle.png', 7, d, 22));
+			deck.push(card('7', 'Waves', 'Wyrms', '', 'the CAVE', false, '7_cave.png', 7, d, 23));
+			deck.push(card('7', 'Moons', 'Leaves', '', 'the CHANCE MEETING', false, '7_chance_meeting.png', 7, d, 24));
+			deck.push(card('8', 'Wyrms', 'Knots', '', 'the BETRAYAL', false, '8_betrayal.png', 8, d, 25));
+			deck.push(card('8', 'Moons', 'Suns', '', 'the DIPLOMAT', true, '8_diplomat.png', 8, d, 26));
+			deck.push(card('8', 'Waves', 'Leaves', '', 'the MILL', false, '8_mill.png', 8, d, 27));
+			deck.push(card('9', 'Waves', 'Wyrms', '', 'the DARKNESS', false, '9_darkness.png', 9, d, 28));
+			deck.push(card('9', 'Leaves', 'Knots', '', 'the MERCHANT', true, '9_merchant.png', 9, d, 29));
+			deck.push(card('9', 'Moons', 'Suns', '', 'the PACT', false, '9_pact.png', 9, d, 30));
+			deck.push(card('CROWN', 'Knots', '', '', 'the WINDFALL', false, 'crown_knots.png', 10, d, 39));
+			deck.push(card('CROWN', 'Leaves', '', '', 'the END', false, 'crown_leaves.png', 10, d, 40));
+			deck.push(card('CROWN', 'Moons', '', '', 'the HUNTRESS', true, 'crown_moons.png', 10, d, 41));
+			deck.push(card('CROWN', 'Suns', '', '', 'the BARD', true, 'crown_suns.png', 10, d, 42));
+			deck.push(card('CROWN', 'Waves', '', '', 'the SEA', false, 'crown_waves.png', 10, d, 43));
+			deck.push(card('CROWN', 'Wyrms', '', '', 'the CALAMITY', false, 'crown_wyrms.png', 10, d, 44));
+			deck.push(card('', '', '', '', 'the EXCUSE', false, 'excuse.png', 0, d, 0));
+			deck.push(card('PAWN', 'Waves', 'Leaves', 'Wyrms', 'the BORDERLAND', false, 'pawn_borderland.png', 11, d, 31));
+			deck.push(card('PAWN', 'Moons', 'Suns', 'Leaves', 'the HARVEST', false, 'pawn_harvest.png', 11, d, 32));
+			deck.push(card('PAWN', 'Suns', 'Waves', 'Knots', 'the LIGHT KEEPER', true, 'pawn_light_keeper.png', 11, d, 33));
+			deck.push(card('PAWN', 'Moons', 'Wyrms', 'Knots', 'the WATCHMAN', true, 'pawn_watchman.png', 11, d, 34));
+			deck.push(card('COURT', 'Moons', 'Waves', 'Knots', 'the CONSUL', true, '11_court_consul.png', 12, d, 35));
+			deck.push(card('COURT', 'Suns', 'Waves', 'Wyrms', 'the ISLAND', false, '11_court_island.png', 12, d, 36));
+			deck.push(card('COURT', 'Moons', 'Leaves', 'Wyrms', 'the RITE', false, '11_court_rite.png', 12, d, 37));
+			deck.push(card('COURT', 'Suns', 'Leaves', 'Knots', 'the WINDOW', false, '11_court_window.png', 12, d, 38));
 		}
 		return deck;
 	}
@@ -161,7 +162,8 @@ context.remove = (function () {
 context.shuffle = (function () {
 
 	return {
-		deck: deck
+		deck: deck,
+		sort: sort
 	};
 
 	function deck(deck) {
@@ -177,6 +179,16 @@ context.shuffle = (function () {
 			deck[n] = temp;
 		}
 		return deck;
+	}
+
+	function sort(deck,invert) {
+		// unshuffle the deck of cards (for testing).
+		return deck.sort(function(a, b) {
+        if (invert)
+					return (b["CardNo"] > a["CardNo"]);
+        else
+					return (a["CardNo"] > b["CardNo"]);
+    });
 	}
 
 	// Removed a suit function that was only useful for Adaman.
